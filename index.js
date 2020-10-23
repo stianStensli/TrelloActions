0db23b7bc9c36336988113c3a1b4c019f108929c
stianStensli <stian.horn.stensli@nav.no>
2020-10-23 14:50:29 +0200

    Get Trello cards

--- index.js
+++ index.js
@@ -2,14 +2,20 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
+    const key = core.getInput('key');
+    const token = core.getInput('token');
     
-    const nameToGreet = core.getInput('who-to-greet');
-    console.log(`Hello ${nameToGreet}!`);
     const time = (new Date()).toTimeString();
-    core.setOutput("time", time);
+    core.setOutput("At time: ", time);
     
     const payload = JSON.stringify(github.context.payload, undefined, 2)
-    console.log(`The event payload: ${payload}`);
+    const message = github.context.payload.head_commit.message
+    
+    fetch(`https://api.trello.com/1/boards/5ef097c91bc451362bac9ac4/cards?fields=name,url,idShort&key=${key}&token=${token}`, { method: 'POST', body: form })
+    .then(res => res.json())
+    .then(json => console.log(json));
+
+    console.log(`The message: ${message}`)
 } catch (error) {
     core.setFailed(error.message);
 }
