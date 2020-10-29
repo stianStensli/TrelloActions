7efcdad1bdaa5f8fd0ac25b4edc80cdf3c7f7e95
stianStensli <stian.hs@hotmail.com>
2020-10-29 10:41:38 +0100

    Async

--- get-card/index.js
+++ get-card/index.js
@@ -4,6 +4,19 @@ const fetch = require('node-fetch');
 
 const trelloCardPattern = /^[T|t][C|c]-(\d+)/;
 
+
+async function apiCall(board, key, token)  {
+    return fetch(`https://api.trello.com/1/boards/${board}/cards?fields=name,url,idShort&key=${key}&token=${token}`, { method: 'GET'})
+        .then(res => res.json())
+        .then(json => {
+            const requestedCard = json.find(v => v.idShort == requestedCardShortId)
+            console.log(`The card: ${requestedCard.id}`)
+            return requestedCard.id
+    }).finally(() => {
+        core.endGroup()
+    });
+}
+
 try {
     const key = core.getInput('key');
     const token = core.getInput('token');
@@ -14,18 +27,12 @@ try {
         const requestedCardShortId = match[1]
         console.log(`Requested short ID: ${requestedCardShortId}`)
 
-        const result = await core.group('Fetching card id', async () => {
-            const response = await fetch(`https://api.trello.com/1/boards/${board}/cards?fields=name,url,idShort&key=${key}&token=${token}`, { method: 'GET'})
-                .then(res => res.json())
-                .then(json => {
-                    const requestedCard = json.find(v => v.idShort == requestedCardShortId)
-                    console.log(`The card: ${requestedCard.id}`)
-                    return requestedCard.id
-                })
+        const result = core.group('Fetching card id', async () => {
+            const response = await apiCall(board, key, token);
             return response
           });
           core.setOutput("card-id", result);
-          
+
     }else{
         console.log("Commit message for Trello link must start with TC-<num>")
     }
